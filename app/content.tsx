8d7f024dca81f2e3bae216f9773a1e4cefda128e
Ling Chang <127913579+hunlin99@users.noreply.github.com>
2024-09-10 18:18:34 +0000

    update0910

--- app/content.tsx
+++ app/content.tsx
@@ -3,17 +3,61 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import ReactPlayer from "react-player";
-import { LineChart, ResponsiveContainer } from "recharts";
-import { MapContainer, TileLayer, Marker, Popup } from 'https://cdn.esm.sh/react-leaflet/MapContainer';
-import "leaflet/dist/leaflet"
+import { LineChart, XAxis, Tooltip, CartesianGrid, Line,  ResponsiveContainer } from "recharts";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
 
+const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
 export default function Content() {
 
     return <>
     <div className="container">
         <div className="row">
             <div className="col-6">
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer style={{height:500}}center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
             <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
@@ -25,10 +69,16 @@ export default function Content() {
             </MapContainer>
             </div>
             <div className="col-6">
-                [CHART GOES HERE]
+            <LineChart width={400} height={400} data={data} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
+            <XAxis dataKey="name" />
+            <Tooltip />
+            <CartesianGrid stroke="#f5f5f5" />
+            <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+            <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+            </LineChart>
             </div>
             <div className="col-12">
-                [ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' /]
+                <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
             </div>
         </div>
     </div>
